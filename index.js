0900bb2b34ac15b9affba34270a70740410fbf94
Wise Kamau <wise@gmail.com>
2026-07-07 22:32:40 +0300

    Complete Flatbooks bookstore DOM lab

--- index.js
+++ index.js
@@ -2,46 +2,57 @@ const bookStore = {
     name: 'Flatbooks Technical Books',
     books: [
         {
-            id:1,
+            id: 1,
             title: 'Eloquent JavaScript: A Modern Introduction to Programming',
             author: 'Marjin Haverbeke',
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-            
         },
         {
-            id:2,
+            id: 2,
             title: 'JavaScript & JQuery: Interactive Front-End Web Development',
             author: 'Jon Duckett',
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
         },
         {
-            id:3,
+            id: 3,
             title: 'JavaScript: The Good Parts',
             author: 'Douglas Crockford',
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
         },
         {
-            id:4,
+            id: 4,
             title: 'JavaScript: The Definitive Guide',
             author: 'David Flanagan',
-            imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
-            
+            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg'
         },
         {
-            id:5,
+            id: 5,
             title: 'You Don’t Know JS',
             author: 'Kyle Simpson',
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
         },
         {
-            id:6,
+            id: 6,
             title: 'Cracking the Coding Interview',
             author: 'Gayle Laakmann McDowell',
             imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
-            
         }
     ]
-}
+};
 
 // Write your code here!
 
+document.getElementById("header").textContent = bookStore.name;
+
+const bookList = document.getElementById("book-list");
+bookList.innerHTML = "";
+
+bookStore.books.forEach(book => {
+    bookList.innerHTML += `
+        <li>
+            <h3>${book.title}</h3>
+            <p>${book.author}</p>
+            <img src="${book.imageUrl}" alt="${book.title}">
+        </li>
+    `;
+});
